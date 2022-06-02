1567cc2649319b5fbfb54ca99860fb84b965bd31
Osman Rafi <33856265+Osman-Rafi@users.noreply.github.com>
2022-06-02 20:19:52 +0600

    chore: xs screen added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'poppins': ['"Poppins"', "sans-serif"],
+      },
+    },
+    screens: {
+      xs: "375px",
+      ...defaultTheme.screens,
+    },
   },
   plugins: [],
 };
